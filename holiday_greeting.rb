a7e25c7a1d541f3186c3a26f8d1c85821eaa21d2
Mary Cotter <marebearc@gmail.com>
2018-07-24 15:51:07 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,4 +1,4 @@
-def 
-Hello
+def holiday_greeting(holiday="Mother's Day", recipient="Mom", sender="Your Favorite Child")
+  "Happy #{holiday}, #{recipient}! From #{sender}"
+end
 
-"Happy Mother's Day, Mom! From Your Favorite Child"
